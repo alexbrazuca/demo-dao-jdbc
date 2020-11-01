8f36978b0988e8ed3ab017b5ba85ddee8c484d3b
Alex Nascimento <alexbrazuca@live.com>
2020-11-01 13:52:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department dp = new Department(1,"books");
 		
 		System.out.println(dp);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+		
+		System.out.println(seller);
 	}
 
 }
